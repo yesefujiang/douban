bb6bdbf0404cf58c69e1e864e56511b3525ad658
yesefujiang <he123789456@gmail.com>
2019-11-17 10:56:32 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, url_for, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+import os, sys, click
+
+WIN = sys.platform.startswith('win')
+if WIN:  # 如果是 Windows 系统，使用三个斜线
+    prefix = 'sqlite:///'
+else:  # 否则使用四个斜线
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['SECRET_KEY'] = 'dev'   #设置密匙
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')   #获取模板中输入字段的name值
+        #验证数据是否有效
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('Invalid input.')   #使用flash函数渲染错误提示框
+            return redirect(url_for('index'))  #重定向到首页
+        #数据有效，保存表单数据到数据库
+        movie = Movie(title=title,year=year)  #创建一条数据记录
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')  #显示创建成功
+        return redirect(url_for('index'))  #返回主页
+    #默认通过get方法获取到主页数据
+    user = User.query.get(1)
+    movies = Movie.query.all()
+    return render_template('index.html')
+
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
+
+@app.errorhandler(404)
+def page_not_found(e):  # 接收异常对象
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return dict(user=user, movies=movies)
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done')
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 标题
+    year = db.Column(db.String(4))  # 年份
