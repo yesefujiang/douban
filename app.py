10eaa04f8b88281977ed4bca8684d0deceec33f9
yesefujiang <he123789456@gmail.com>
2019-11-17 17:21:30 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, url_for, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os, sys, click
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
@@ -11,27 +13,30 @@ else:  # 否则使用四个斜线
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = 'dev'   #设置密匙
+app.config['SECRET_KEY'] = 'dev'  # 设置密匙
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:  # 如果当前用户未认证
+            return redirect(url_for('index'))  # 重定向到主页
         title = request.form.get('title')
-        year = request.form.get('year')   #获取模板中输入字段的name值
-        #验证数据是否有效
-        if not title or not year or len(year)>4 or len(title)>60:
-            flash('Invalid input.')   #使用flash函数渲染错误提示框
-            return redirect(url_for('index'))  #重定向到首页
-        #数据有效，保存表单数据到数据库
-        movie = Movie(title=title,year=year)  #创建一条数据记录
+        year = request.form.get('year')  # 获取模板中输入字段的name值
+        # 验证数据是否有效
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 使用flash函数渲染错误提示框
+            return redirect(url_for('index'))  # 重定向到首页
+        # 数据有效，保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建一条数据记录
         db.session.add(movie)
         db.session.commit()
-        flash('Item created.')  #显示创建成功
-        return redirect(url_for('index'))  #返回主页
-    #默认通过get方法获取到主页数据
+        flash('Item created.')  # 显示创建成功
+        return redirect(url_for('index'))  # 返回主页
+    # 默认通过get方法获取到主页数据
     return render_template('index.html')
 
 @app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
@@ -54,13 +59,16 @@ def edit(movie_id):
 
     return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
 
+
 @app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+@login_required
 def delete(movie_id):
     movie = Movie.query.get_or_404(movie_id)
     db.session.delete(movie)
     db.session.commit()
     flash('Item deleted.')
     return redirect(url_for('index'))
+
 
 @app.errorhandler(404)
 def page_not_found(e):  # 接收异常对象
@@ -73,6 +81,39 @@ def inject_user():
     user = User.query.first()
     movies = Movie.query.all()
     return dict(user=user, movies=movies)
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+@app.cli.command()
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True,
+              help='The password used to login.')
+def admin(username, password):
+    """Create user."""
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user...')
+        user.username = username
+        user.set_password(password)  # 设置密码
+    else:
+        click.echo('Creating user...')
+        user = User(username=username, name='Admin')
+        user.set_password(password)  # 设置密码
+        db.session.add(user)
+
+    db.session.commit()  # 提交数据库会话
+    click.echo('Done.')
 
 
 @app.cli.command()
@@ -102,12 +143,78 @@ def forge():
     click.echo('Done')
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)  # 主键
     name = db.Column(db.String(20))  # 名字
+    username = db.Column(db.String(20))  # 用户名
+    password_hash = db.Column(db.String(128))  # 密码散列值
+
+    def set_password(self, password):  # 用来设置密码的方法，接受密码作为参数，生成散列值
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)  # 验证密码是否一致，返回布尔值
 
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # 主键
     title = db.Column(db.String(60))  # 标题
     year = db.Column(db.String(4))  # 年份
+
+
+@login_manager.user_loader
+def load_user(user_id):  # 创建用户加载回调函数，接收用户id参数
+    user = User.query.get(int(user_id))
+    return user
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash('Invalid input.')
+            return redirect(url_for('login'))
+
+        user = User.query.first()
+        # 验证用户名和密码是否一致
+        if username == user.username and user.validate_password(password):
+            login_user(user)  # 登入用户
+            flash('Login success.')
+            return redirect(url_for('index'))  # 重定向到主页
+
+        flash('Invalid username or password.')  # 如果验证失败，显示错误消息
+        return redirect(url_for('login'))  # 重定向回登录页面
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()  # 登出
+    flash('Goodbye')
+    return redirect(url_for('index'))  # 重定向到首页
+
+@app.route('/settings', methods=['GET', 'POST'])
+@login_required
+def settings():
+    if request.method == 'POST':
+        name = request.form['name']
+
+        if not name or len(name) > 20:
+            flash('Invalid input.')
+            return redirect(url_for('settings'))
+
+        current_user.name = name
+        # current_user 会返回当前登录用户的数据库记录对象
+        # 等同于下面的用法
+        # user = User.query.first()
+        # user.name = name
+        db.session.commit()
+        flash('Settings updated.')
+        return redirect(url_for('index'))
+
+    return render_template('settings.html')
